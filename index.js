d30321fd1f3c5ea0a9794e0f5b04346473f5452c
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2024-07-18 22:27:31 +0700

    Cấu hình MVC

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+ 
+const route = require("./routes/client/index.route")
+
+const app = express();
+const port = 3000;
+
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+// Routes
+route(app);
+
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+})
