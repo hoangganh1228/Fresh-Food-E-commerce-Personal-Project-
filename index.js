49f5036ef807709151d75d6e4f86937b0e47273e
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2024-07-21 16:06:44 +0700

    1.3. Làm giao diện danh sách sản phẩm

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("dotenv").config();
 
 const database = require("./config/database")
+const systemConfig = require("./config/system")
 
 const route = require("./routes/client/index.route")
 const routeAdmin = require("./routes/admin/index.route")
@@ -15,6 +16,8 @@ app.use(express.static("public"));
 
 app.set("views", "./views");
 app.set("view engine", "pug");
+
+app.locals.prefixAdmin = systemConfig.prefixAdmin;
 
 // Routes
 route(app);
