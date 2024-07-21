cbd11c759a380ed4e13703a27789c9b71abc601f
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2024-07-21 21:01:49 +0700

    3. Tính năng lọc theo trạng thái

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,8 +1,38 @@
 const Product = require("../../models/products.model")
 
 module.exports.index = async (req, res) => {
+    let filterStatus = [
+        {
+            name: "Tất cả",
+            status: "",
+            class: ""
+        },
+        {
+            name: "Hoạt động",
+            status: "active",
+            class: ""
+        },
+        {
+            name: "Dừng hoạt động",
+            status: "inactive",
+            class: ""
+        }
+    ]
+
+    if(req.query.status) {
+        const index = filterStatus.findIndex(item => item.status == req.query.status);
+        filterStatus[index].class = "active"
+    } else {
+        const index = filterStatus.findIndex(item => item.status == "");
+        filterStatus[index].class = "active"
+    }
+
     let find = {
         deleted: "false",
+    }
+
+    if(req.query.status) {
+        find.status = req.query.status;
     }
 
     const products = await Product.find(find);
@@ -10,6 +40,7 @@ module.exports.index = async (req, res) => {
 
     res.render("admin/pages/products/index", {
         pageTitle: "Trang sản phẩm",
-        products: products
+        products: products,
+        filterStatus: filterStatus
     })
 }
