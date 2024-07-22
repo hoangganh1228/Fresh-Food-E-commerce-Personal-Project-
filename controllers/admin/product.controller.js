bb5a797a67a4e39b497251df5e29f78ef3ad5ed3
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2024-07-22 16:16:53 +0700

    4. Tính năng tìm kiếm

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -35,12 +35,21 @@ module.exports.index = async (req, res) => {
         find.status = req.query.status;
     }
 
+    let keyword = "";
+    if(req.query.keyword) {
+        keyword = req.query.keyword;
+        // console.log(keyword);
+        const regex = new RegExp(keyword, "i")
+        find.title = regex;
+    }
+
     const products = await Product.find(find);
     // console.log(products);
 
     res.render("admin/pages/products/index", {
         pageTitle: "Trang sản phẩm",
         products: products,
+        keyword: keyword,
         filterStatus: filterStatus
     })
 }
--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -15,3 +15,22 @@ if(buttonStatus.length > 0) {
         })
     })
 }
+
+// Form Search
+
+const formSearch = document.querySelector("#form-search")
+if(formSearch) {
+    let url = new URL(window.location.href)
+    formSearch.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const keyword = e.target.elements.keyword.value;
+        if(keyword) {
+            url.searchParams.set("keyword", keyword)
+        } else {
+            url.searchParams.delete("keyword")
+        }
+
+        window.location.href = url.href
+    })
+}
+
