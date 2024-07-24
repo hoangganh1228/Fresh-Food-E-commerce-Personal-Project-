08f40bcd18f633c9689d62c28fd096555d9f9d4a
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2024-07-24 10:46:12 +0700

    Làm trang chi tiết sản phẩm bên admin

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -190,3 +190,23 @@ module.exports.editPatch = async (req, res) => {
     }
     res.redirect("back");
 }
+
+// [GET] /admin/products/detail/:id
+module.exports.detail = async (req, res) => {
+    try {
+        const find = {
+            deleted: false,
+            _id: req.params.id
+        }
+    
+        const product = await Product.findOne(find);
+    
+        res.render("admin/pages/products/details", {
+            pageTitle: product.title,
+            product: product  
+        });
+        
+    } catch (error) {
+        res.redirect(`${systemConfig.prefixAdmin}/products`)   
+    }
+}
