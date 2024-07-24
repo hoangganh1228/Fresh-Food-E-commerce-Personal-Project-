ca5358abf9dc25834323d313e385e103ea0ebaab
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2024-07-24 10:38:14 +0700

    Tính năng chỉnh sửa sản phẩm

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -148,3 +148,45 @@ module.exports.createPost = async (req, res) => {
 
     res.redirect(`${systemConfig.prefixAdmin}/products`)
 }
+
+// [GET] /admin/products/edit
+module.exports.edit = async (req, res) => {
+    try {
+        const find = {
+            deleted: false,
+            _id: req.params.id
+        }
+    
+        const product = await Product.findOne(find);
+    
+        res.render("admin/pages/products/edit", {
+            pageTitle: "Chỉnh sửa sản phẩm",
+            product: product  
+        });
+        
+    } catch (error) {
+        req.flash("error", `Không tồn tại sản phẩm này!`);
+
+        res.redirect(`${systemConfig.prefixAdmin}/products`)       
+    }
+}
+
+module.exports.editPatch = async (req, res) => {
+    const id = req.params.id;
+    req.body.price = parseInt(req.body.price);
+    req.body.discountPercentage = parseInt(req.body.discountPercentage);
+    req.body.stock = parseInt(req.body.stock);
+    req.body.position = parseInt(req.body.position)
+
+    if(req.file) {
+        req.body.thumbnail = `/uploads/${req.file.filename}`
+    }
+    
+    try {
+        await Product.updateOne( { _id: id}, req.body)
+        req.flash("success", `Cập nhật thành công sản phẩm!`);
+    } catch (error) {
+        req.flash("error", `Cập nhật thất bại!`);
+    }
+    res.redirect("back");
+}
--- routes/admin/products.route.js
+++ routes/admin/products.route.js
@@ -23,6 +23,16 @@ router.post(
     upload.single('thumbnail'),    
     validate.createPost,
     controller.createPost,
-    );
+    
+);
+
+router.get("/edit/:id", controller.edit);
+
+router.patch(
+    "/edit/:id", 
+    upload.single('thumbnail'),    
+    validate.createPost,
+    controller.editPatch
+);
 
 module.exports = router;
