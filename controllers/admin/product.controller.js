139a793224428e0f6d9c4cf32c21024906f3494c
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2024-07-24 09:25:28 +0700

    4. Tính năng tạo mới sản phẩm (Upload ảnh)

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -137,6 +137,7 @@ module.exports.createPost = async (req, res) => {
         req.body.position = parseInt(req.body.position);
     }
 
+    req.body.thumbnail = `/uploads/${req.file.filename}`
 
     const product = new Product(req.body);
     await product.save();
