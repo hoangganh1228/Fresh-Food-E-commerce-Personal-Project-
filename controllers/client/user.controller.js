b3f745b7e4f2eba578714da2d051dbadbcfeb855
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2024-08-27 14:34:36 +0700

    Đăng ký tài khoản

---
+++ controllers/client/user.controller.js
@@ -0,0 +1,36 @@
+const md5 = require("md5");
+const User = require("../../models/user.model");
+const { exists } = require("../../models/cart.model");
+
+// [GET] /user/register
+module.exports.register = async (req, res) => {
+  res.render("client/pages/user/register", {
+    pageTitle: "Đăng ký tài khoản",
+  });
+};
+
+// [POST] /user/register
+module.exports.registerPost = async (req, res) => {
+  // console.log(req.body);
+
+  const existEmail  = await User.findOne({
+    email: req.body.email
+  })
+
+  if(existEmail ) {
+    req.flash("error", "Email đã tồn tại!");
+    res.redirect("back");
+    return;
+  }
+
+  req.body.password = md5(req.body.password);
+  const user = new User(req.body);
+  user.save();
+
+  res.cookie("tokenUser", user.tokenUser);
+
+  res.redirect("/");
+
+  res.send("OK");
+  
+}
