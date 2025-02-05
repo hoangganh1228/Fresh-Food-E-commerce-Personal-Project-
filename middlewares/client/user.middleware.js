8b75f31088b253d2521272f73e6ab5b3538eeda8
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2025-02-05 11:10:45 +0700

    README

--- middlewares/client/user.middleware.js
+++ middlewares/client/user.middleware.js
@@ -1,17 +1,35 @@
 const User = require("../../models/user.model");
+const Key = require("../../models/key.model");
+const {verifyToken} = require("../../utils/jwtHelper")
 
 module.exports.infoUser = async (req, res, next) => {
-  if(req.cookies.tokenUser) {
-    const user = await User.findOne({
-      tokenUser: req.cookies.tokenUser,
-      deleted: false,
-      status: "active"
-    }).select("-password");
+  const token = req.cookies.refreshToken;
+  console.log(token);
+  
+  const key = await Key.findOne({
+    refreshToken: token
+  })
+  // if (!key) {
+  //   req.flash("error", "Không tìm thấy khóa bảo mật, hãy đăng nhập lại!");
+  //   return res.redirect("/user/login");
+  // }
 
-    if(user) {
-      res.locals.user = user;
-    }
+  console.log(key);
+  
+
+  const decoded = verifyToken(token, key.publicKey);
+  // if (!decoded) {
+  //   req.flash("error", "Phiên đăng nhập đã hết hạn, vui lòng đăng nhập lại!");
+  //   return res.redirect("/user/login"); 
+  // }
+  console.log(decoded);
+  
+  const user = await User.findById(decoded.userId).select("-password");
+  if (user) {
+    res.locals.user = user;
+    req.userId = decoded.userId;
   }
-
+  
+  
   next();
 }
